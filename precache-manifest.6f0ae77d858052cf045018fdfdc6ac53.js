c2efaa51bc7537fc92624bee5d8bc2f0929449a6
tkrogers3 <tkrogers3@gmail.com>
2020-03-31 16:34:51 -0400

    Updates

---
+++ precache-manifest.6f0ae77d858052cf045018fdfdc6ac53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c2af70cb9fbf1cfc2c11980c39a0bb",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "24c394c859b544d9dce3",
+    "url": "/react-restaurant/static/css/2.81e2c8a5.chunk.css"
+  },
+  {
+    "revision": "7f1a0e35b3e2e8ea83b9",
+    "url": "/react-restaurant/static/css/main.e42ee4b7.chunk.css"
+  },
+  {
+    "revision": "24c394c859b544d9dce3",
+    "url": "/react-restaurant/static/js/2.2be6c020.chunk.js"
+  },
+  {
+    "revision": "bc51ad6b075e5b96cf9ad2ad3d22ac0d",
+    "url": "/react-restaurant/static/js/2.2be6c020.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f1a0e35b3e2e8ea83b9",
+    "url": "/react-restaurant/static/js/main.e27d8cb9.chunk.js"
+  },
+  {
+    "revision": "e5b8f0956db9a1004e3a",
+    "url": "/react-restaurant/static/js/runtime-main.503e9388.js"
+  }
+]);
